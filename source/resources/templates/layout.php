f86a9504dbc5cd616f63a94f409f4a7f75a3825c
thanhle1547 <thanhlm72@wru.vn>
2020-04-22 19:23:40 +0700

    Created templates & view resource files

---
+++ source/resources/templates/layout.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <?= $this->fetch('head.php'); ?>
+
+<body>
+    <?= $this->fetch('header.php'); ?>
+
+    <?= $content; ?>
+
+    <?= $this->fetch('footer.php'); ?>
+</body>
+
+</html>
