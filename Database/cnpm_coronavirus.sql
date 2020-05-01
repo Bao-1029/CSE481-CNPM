44d06efa80138822df67d8092a2ffd7ee6be1db9
thanhle1547 <thanhlm72@wru.vn>
2020-05-01 13:40:59 +0700

    Added 2 tables: hospital_hotlines, user

--- Database/cnpm_coronavirus.sql
+++ Database/cnpm_coronavirus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th4 30, 2020 lúc 12:04 PM
+-- Thời gian đã tạo: Th5 01, 2020 lúc 08:33 AM
 -- Phiên bản máy phục vụ: 10.1.38-MariaDB
 -- Phiên bản PHP: 7.3.4
 
@@ -68,6 +68,38 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
+-- Cấu trúc bảng cho bảng `hospital_hotlines`
+--
+
+CREATE TABLE `hospital_hotlines` (
+  `id` tinyint(3) UNSIGNED NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone_number` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `hospital_hotlines`
+--
+
+INSERT INTO `hospital_hotlines` (`id`, `name`, `phone_number`) VALUES
+(1, 'Bệnh viện Bạch Mai', '0969.851.616'),
+(2, 'Bệnh viện Nhiệt đới Trung ương', '0969.241.616'),
+(3, 'Bệnh viện E', '0912.168.887'),
+(4, 'Bệnh viện Nhi trung ương', '0372.884.712'),
+(5, 'Bệnh viện Phổi trung ương', '0967.941.616'),
+(6, 'Bệnh viện Việt Nam – Thụy Điển Uông Bí', '0966.681.313'),
+(7, 'Bệnh viện Đa khoa trung ương Thái Nguyên', '0913.394.495'),
+(8, 'Bệnh viện Trung ương Huế', '0965.301.212'),
+(9, 'Bệnh viện Chợ Rẫy', '0969.871.010'),
+(10, 'Bệnh viện Đa khoa trung ương Cần Thơ', '0907.736.736'),
+(11, 'Bệnh viện Xanh Pôn Hà Nội', '0904.138.502'),
+(12, 'Bệnh viện Vinmec Hà Nội', '0934.472.768'),
+(13, 'Bệnh viện Đà Nẵng', '0903.583.881'),
+(14, 'Bệnh viện Nhiệt đới TP.HCM', '0967.341.010 ');
+
+-- --------------------------------------------------------
+
+--
 -- Cấu trúc bảng cho bảng `news`
 --
 
@@ -111,12 +143,19 @@ CREATE TABLE `news_source` (
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` tinyint(3) UNSIGNED NOT NULL,
   `userName` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` tinytext COLLATE utf8mb4_unicode_ci NOT NULL,
   `level` tinyint(4) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `users`
+--
+
+INSERT INTO `users` (`id`, `userName`, `password`, `level`, `status`) VALUES
+(1, 'admin', '$2y$10$gZfmlD/1e.dWoAnsHiJw1.5EuPBJgyXvg/5ze/WtZPSx.K30YvpOi', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -130,6 +169,12 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 -- Chỉ mục cho các bảng đã đổ
 --
+
+--
+-- Chỉ mục cho bảng `hospital_hotlines`
+--
+ALTER TABLE `hospital_hotlines`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Chỉ mục cho bảng `news`
@@ -156,6 +201,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT cho bảng `hospital_hotlines`
+--
+ALTER TABLE `hospital_hotlines`
+  MODIFY `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
 -- AUTO_INCREMENT cho bảng `news`
 --
 ALTER TABLE `news`
@@ -166,6 +217,12 @@ ALTER TABLE `news`
 --
 ALTER TABLE `news_source`
   MODIFY `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
