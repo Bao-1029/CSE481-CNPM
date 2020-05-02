83dd0784fcee29f4758de9dee27019769b65f48d
thanhle1547 <thanhlm72@wru.vn>
2020-05-02 14:24:13 +0700

    Extract admin pages
    - Added missing `breaks` statement in footer view

---
+++ source/resources/views/admin/login.php
@@ -0,0 +1,15 @@
+<main class="main">
+    <div class="main__login">
+        <form action="" class="main__login-form">
+            <h2 class="main__title">Đăng nhập</h2>
+            <label for="">Tài khoản</label>
+            <input type="text" required>
+
+            <label for="">Mật khẩu</label>
+            <input type="password" required>
+
+            <button type="submit">Đăng nhập</button>
+        </form>
+    </div>
+
+</main>
---
+++ source/resources/views/admin/partials/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <p>A project by 59Th2 | Thuy Loi University</p>
+</footer>
+
+<script src="js/hotlines.js" type="module"></script>
